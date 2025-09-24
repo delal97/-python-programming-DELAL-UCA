e5f8deccf9b5c94ee01eeecc0fa35ff0a7082384
delal97 <Delal.uca@iths.se>
2025-09-24 10:04:38 +0200

    Update ecercises0.py

--- exercise/ecercises0.py
+++ exercise/ecercises0.py
@@ -1,1 +1,52 @@
+a)
+import math
+a = 3
+b = 4
+c = math.sqrt(a**2 + b**2)
+print(c)
+
+b)
+import math
+c = 7.0
+a = 5.0
+b = math.sqrt(c**2 - a**2)
+print(b)
+
+fråga 2
+totat_predictions = 365
+accurate_predictions = 300
+accuracy_rate = (accurate_predictions/total_predictions)*100
+print(accuracy_rate)
+
+fråga 3
+accurate_predictions=(2 + 985)
+total_predictions=(2 + 985 + 11 + 2)
+accuracy=(accurate_predictions/total_predictions)*100
+print(accuracy)
+
+fråga 4
+a = (4,4)
+b = (0,1)
+k = (a[1] - b[1]) / (a[0] - b[0])
+print(k) 
+
+fråga 5
+import math
+p1 = (3)
+p2 = (5)
+q1 = (-2)
+q2 = (4)
+
+m = math.sqrt((p1 - q1)**2 + (p2 - q2)**2)
+print(m)
+
+feåga 6
+import math
+
+(p1, p2, p3) = (2,1,4)
+(q1, q2, q3) = (3,1,0)
+m = math.sqrt((p1 - q1)**2 + (p2 - q2)**2 + (p3 - q3)**2)
+print(m)
+
+
 
